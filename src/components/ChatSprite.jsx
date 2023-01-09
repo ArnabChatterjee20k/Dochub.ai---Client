2d49278d8aad038e95d3f29915f64b3ffa73edd8
arnab <arnabchatterjee.ac.2@gmail.com>
2023-01-09 19:49:02 +0530

    added copy option to chat bot

--- src/components/ChatSprite.jsx
+++ src/components/ChatSprite.jsx
@@ -2,7 +2,8 @@ import Box from "@mui/material/Box";
 import styled from "@emotion/styled";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Loader from "./Loader";
-
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 const ChatSpriteWrapper = styled(Box)({
   padding: "15px",
   width: "100%",
@@ -11,10 +12,15 @@ const ChatSpriteWrapper = styled(Box)({
   gap: 4,
 });
 
+const copyData = (data) => {
+  navigator.clipboard.writeText(data).then(()=>alert("Text copied"));
+};
+
 export function AiChatSprite({ chat }) {
   return (
-    <ChatSpriteWrapper sx={{ backgroundColor: "#e0f7fa47" }}>
-      {chat.trim().length === 0 ? <Loader /> : chat}
+    <ChatSpriteWrapper sx={{ backgroundColor: "#e0f7fa47" }} flexDirection="column" gap={1}>
+      {chat.trim().length === 0 ? <Loader /> : <Typography>{chat}</Typography>}
+      {chat.trim().length !== 0 && <Button variant="contained" sx={{alignSelf:"flex-start"}} onClick={()=>copyData(chat)}>Copy</Button>}
     </ChatSpriteWrapper>
   );
 }
